dd8e747e49108d82aaae520b505650bfac7a6e2b
ROMANVIKI <vikiadhi2016@gmail.com>
2025-05-17 14:26:08 +0530

    title change

--- dist/static/chunks/app_page_jsx_fbdaf659._.js
+++ dist/static/chunks/app_page_jsx_fbdaf659._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/app_globals_73c37791.css"
+  "static/chunks/node_modules_056c3173._.js",
+  "static/chunks/components_Navbar_jsx_8441bf06._.js"
 ],
     source: "dynamic"
 });
